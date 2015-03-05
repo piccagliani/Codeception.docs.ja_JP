d19ff4232aebeb024cca1ff68a6d7042d2f71342
piccagliani <piccagliani.dev@gmail.com>
2015-03-05 18:09:38 +0900

    サイトにナビゲーションを追加するための修正

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-rm -f ./site/*.html
-sed -i -e "1i ---\n---" *.md
+sed -i -e "1i ---\nlayout: guides\n---" *-*.md
+sed -i -e "1i ---\nlayout: default\n---" index.md
+sed -i -e "1i ---\nlayout: default\n---" README.md
 jekyll build
-git checkout -- .
+git checkout -- *.md
+cp -r _assets/images _site/
+cp -r _assets/javascripts _site/
+cp -r _assets/stylesheets _site/
